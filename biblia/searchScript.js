d2c2efe5776d4bea3dea2ed371d3171edee6cea4
Eduardo Bach <eduardo-bach@protonmail.com>
2023-02-14 15:23:40 -0300

    textUpdate

--- biblia/searchScript.js
+++ biblia/searchScript.js
@@ -1,33 +1,32 @@
 function achar(){
     var genesis = document.getElementById("gnBible")
     var see = document.getElementById("bibleText")
+    var verse = document.getElementById("gnVerseBible")
+    var verseValue = verse.value;
+    var genesisReal = genesis.value
 
-    var gnURL = `https://www.abibliadigital.com.br/api/verses/nvi/gn/1`
+    var gnURL = `https://www.abibliadigital.com.br/api/verses/nvi/gn/${genesisReal}/${verseValue}`
 
     fetch(gnURL).then((resp)=>{
         return resp.json()
     }).then((data)=>{
-        
-      var caps = genesis.value
-      var realCaps = caps - 1
-        
-        see.innerHTML = data.verses[realCaps].text
+        see.innerHTML = data.text;
     })
     
 }
 
 function acharEx(){
-    var exodo = document.getElementById("exBible")
-    var see = document.getElementById("bibleTextex")
+   var exodo = document.getElementById("exBible")
+   var see = document.getElementById("bibleTextex")
+   var verse = document.getElementById("exVerseBible")
+   var verseReal = verse.value;
+   var exodoReal = exodo.value;
 
-    var exURL = `https://www.abibliadigital.com.br/api/verses/nvi/ex/1`
+   var exURL = `https://www.abibliadigital.com.br/api/verses/nvi/gn/${exodoReal}/${verseReal}`
 
-    fetch(exURL).then((resp)=>{
-        return resp.json()
-    }).then((data)=>{
-        var caps = exodo.value;
-        var realCaps = caps - 1
-
-        see.innerHTML = data.verses[realCaps].text
-    })
+   fetch(exURL).then((resp)=>{
+    return resp.json()
+   }).then((data)=>{
+    see.innerHTML = data.text;
+   })
 }
